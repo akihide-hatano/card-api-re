e4e3f99b1b27817ff8e9465ad3d87f9d63587979
akihide-hatano <a.h.19841202@gmail.com>
2025-09-27 14:25:33 +0900

    設定をcommit

---
+++ app/Models/Card.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    protected $table = 'cards';
+    protected $primaryKey = 'card_id';
+    public $timestamps = true; // デフォルトtrue。falseにするとcreated_at/updated_atを無視
+
+    // 一括代入を許可するカラムを明示
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    protected $casts = [
+    'published_at' => 'datetime',
+    'is_active' => 'boolean',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
