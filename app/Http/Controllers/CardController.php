179b6004db12fe6f4a66ac710b76ee81f6f32421
akihide-hatano <a.h.19841202@gmail.com>
2025-09-27 14:27:58 +0900

    CardControllerのindexを修正

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
 use Illuminate\Http\Request;
 
 class CardController extends Controller
@@ -11,7 +12,9 @@ class CardController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(
+            Card::latest('id')->get(),200
+        );
     }
 
     /**
